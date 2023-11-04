cac79edc0a2564efd4180f3319c34923e253eb41
ViniciusBandeira <viniciusbbandeira@outlook.com>
2023-11-04 00:21:53 -0300

    Page ChoiceVeiculo Add

---
+++ front-end/src/pages/ChoiceVeiculo/index.jsx
@@ -0,0 +1,46 @@
+import '../../App.css'
+import { Body } from '../../components/Body.Style'
+import { BodyContainer, CabContainer, ImgDiv, TitleDiv, TitleText } from '../../components/ConteinerCentral.style'
+import { CenterTitle, Exit, ImgCar, StyleHeader, Title, ImageCarDiv } from '../../components/Header.Style'
+import { Paragraph } from '../../components/TextCentral.Style'
+import {ChoiceButton, ChoiceDiv } from '../../components/Button.style'
+
+function index() {
+
+  return (
+    <>
+      <StyleHeader>
+        <Title>Projeto APMV</Title>
+        <ImageCarDiv>
+          <ImgCar src="./images/car.png" />
+          <CenterTitle>Bem Vindo!</CenterTitle>
+        </ImageCarDiv>
+        <Exit>Sair</Exit>
+      </StyleHeader>
+      <Body>
+        <center>
+          <CabContainer>
+            <TitleDiv>
+              <TitleText> Teste de bateria veicular</TitleText>
+              <ImgDiv>
+                <img src="./images/Bateria.png" width={78} height={66} />
+              </ImgDiv>
+            </TitleDiv>
+
+          </CabContainer>    
+          <BodyContainer>
+            <Paragraph>Click nas opções abaixo para prosseguir...</Paragraph>
+              <ChoiceDiv>
+                <ChoiceButton>Selecionar veículo</ChoiceButton>
+                <ChoiceButton>Cadastrar novo veículo</ChoiceButton>
+              </ChoiceDiv>
+              
+          </BodyContainer>
+        </center>
+      </Body>
+
+    </>
+  )
+}
+
+export default index
